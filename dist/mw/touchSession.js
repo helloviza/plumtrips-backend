c27d73dae761fcdb89727acb6d8bdb772332dd20
helloviza <hellovizawebsite@gmail.com>
2025-09-01 04:20:56 +0530

    chore: add .gitignore to ignore dist, node_modules, and env files

--- dist/mw/touchSession.js
+++
@@ -1,7 +0,0 @@
-import Session from "../models/session.model.js";
-export default async function touchSession(req, _res, next) {
-    if (req.sessionId) {
-        await Session.updateOne({ _id: req.sessionId }, { $set: { lastSeenAt: new Date() } });
-    }
-    next();
-}
